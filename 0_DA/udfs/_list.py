fe60928db8c194b0049e51d951d470b10a382e2e
莫非 <313303303@qq.com>
2018-02-13 14:18:58 +0800

    Update _list.py

--- 0_DA/udfs/_list.py
+++ 0_DA/udfs/_list.py
@@ -1,1 +1,8 @@
+# coding: utf-8
+__title__ = 'list'
+__author__ = 'JieYuan'
+__mtime__ = '2018/2/13'
 
+from collections import OrderedDict
+
+order_set = lambda x: list(OrderedDict.fromkeys(x)) # 列表有序去重
