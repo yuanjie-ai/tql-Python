06934ca89a2c18dee3222e025a277b36ef1152b6
莫非 <313303303@qq.com>
2018-02-13 14:12:55 +0800

    Update _string.py

--- 0_DA/udfs/_string.py
+++ 0_DA/udfs/_string.py
@@ -1,1 +1,9 @@
+# coding: utf-8
 
+def startswith(ls, pattern):
+    """
+    :param ls: 字符串列表
+    :param pattern: 正则表达式
+    :return: 匹配到的规定开头字符串列表
+    """
+    return [i for i in ls if re.match(pattern, i)]
