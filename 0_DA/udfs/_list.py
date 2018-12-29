4d19fd221510a745e0be5f65655c896c79de19a3
尐 <313303303@qq.com>
2018-12-29 13:57:25 +0800

    Delete _list.py

--- 0_DA/udfs/_list.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-__title__ = 'list'
-__author__ = 'JieYuan'
-__mtime__ = '2018/2/13'
-
-from collections import OrderedDict
-
-order_set = lambda x: list(OrderedDict.fromkeys(x)) # 列表有序去重
